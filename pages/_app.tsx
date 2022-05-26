629f41d36ab634171f6c34b29b26ed01d44da022
leoachear <leoachear@gmail.com>
2022-05-26 07:44:32 -0300

    First version of the theme and the projects grid.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,21 @@
 import React from "react";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, Container } from "@chakra-ui/react";
 import { AppProps } from "next/app";
+
+import theme from "../theme";
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
+    <ChakraProvider theme={theme}>
+      <Container
+        backgroundColor="white"
+        boxShadow="md"
+        marginY={4}
+        maxWidth="container.xl"
+        padding={4}
+      >
+        <Component {...pageProps} />
+      </Container>
     </ChakraProvider>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,15 @@ import { GetStaticProps } from "next";
 
 import { Project } from "../project/types";
 import api from "../project/api";
+import {
+  Box,
+  Button,
+  Grid,
+  Heading,
+  Image,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
 
 // types
 interface Props {
@@ -10,7 +19,42 @@ interface Props {
 }
 
 const IndexRoute: React.FC<Props> = ({ projects }) => {
-  return <div>{JSON.stringify(projects)}</div>;
+  return (
+    <Box p="6">
+      <Heading as="h2">Projects</Heading>
+      <Grid templateColumns="repeat(auto-fill, minmax(240px, 1fr))" gridGap="6">
+        {projects.map((project) => {
+          return (
+            <Stack
+              borderRadius="md"
+              padding={4}
+              key={project.id}
+              backgroundColor="gray.100"
+              spacing={5}
+            >
+              <Image src={project.image} alt={project.title} />
+              <Heading as="h3" size="md">
+                {project.title}
+              </Heading>
+              <Stack spacing={1}>
+                <Heading as="h4" size="sm">
+                  Description:
+                </Heading>
+                <Text>{project.description}</Text>
+                <Heading as="h4" size="sm">
+                  Tech stack:
+                </Heading>
+                <Text fontSize="sm" fontWeight="500" color="primary.500">
+                  {project.technologies}
+                </Text>
+              </Stack>
+              <Button colorScheme="primary">See more...</Button>
+            </Stack>
+          );
+        })}
+      </Grid>
+    </Box>
+  );
 };
 
 // using getStaticProps from NextJS
